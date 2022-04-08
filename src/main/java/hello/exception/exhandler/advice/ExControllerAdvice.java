30c74200e4602b39e9b480c5af396fd062d0c5ae
jihye.seong <jihye.seong@ipageon.com>
2022-04-08 16:05:45 +0900

    [API 예외 처리] 스프링이 제공하는 ExceptionResolver
    스프링 부트가 기본으로 제공하는 ExceptionResolver 는 다음과 같다.
    
    HandlerExceptionResolverComposite 에 다음 순서로 등록
    
    1. ***ExceptionHandlerExceptionResolver***
    2. ***ResponseStatusExceptionResolver***
    3. ***DefaultHandlerExceptionResolver*** 우선 순위가 가장 낮다.
    
    그 중 ExceptionHandlerExceptionResolver 에 대해
    -> @ExceptionHandler 를 사용한 API 예외 처리
    -> @ControllerAdvice 를 사용한 글로벌 APi 예외 처리 적용

---
+++ src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,45 @@
+package hello.exception.exhandler.advice;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "hello.exception.api")
+public class ExControllerAdvice {
+
+    /**
+     * 해당 컨트롤러에서 IllegalArgumentException 예외가 발생하면
+     * @ExceptionHandler 가 에러를 잡고 아래 메소드 로직이 호출됨.
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 위의 두 @ExceptionHandler 메소드에서 잡지 못하는 에러는
+     * 아래의 에러메소드로 잡을 수 있다.
+     * Exception 은 가장 최상위의 에러클래스이기 때문이다.
+     */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+}
